e1127933a1a8d3aa90bbcfefccb2b6f172a229f0
MahmoudHafezDarwish <mahm.dar99@gmail.com>
2022-09-20 11:27:36 +0300

    design e-book store commit

---
+++ lib/pages/auth/privacy_policy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PrivayPolicyScreen extends StatelessWidget {
+  const PrivayPolicyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
